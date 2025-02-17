63e6be8ec0b8d07ccef371b57096da6248871e89
Yassir JAAIDI <157497490+Yasser-JD@users.noreply.github.com>
2025-02-17 01:47:32 +0100

    #

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 
-import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaYoutube />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/Yasser-JD" },
+  { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/yassir-jaaidi-18224533a" }
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
